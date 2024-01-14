030f22dcc555e3837abf51335f8edc5775eac00e
toan nguyen <toannguyen@toans-MacBook-Pro.local>
2024-01-14 12:46:31 +0700

    first commit

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,30 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.acme.services.GreetingService;
+
+@Path("/hello")
+public class GreetingResource {
+
+    private final GreetingService greetingService;
+
+    public GreetingResource(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+        return greetingService.greeting(name);
+    }
+}
---
+++ src/main/java/org/acme/services/GreetingService.java
@@ -0,0 +1,10 @@
+package org.acme.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+}
