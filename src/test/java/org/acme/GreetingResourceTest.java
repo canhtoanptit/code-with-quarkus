52a3731a890e8d6f88881b2fe26e7d93292b1e6a
toan nguyen <toannguyen@toans-MacBook-Pro.local>
2024-01-14 13:16:31 +0700

    correct test

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -23,6 +23,11 @@ class GreetingResourceTest {
     void testGreetingEndpoint() {
         String uuid = UUID.randomUUID().toString();
         given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello " + uuid));
     }
 
 }
